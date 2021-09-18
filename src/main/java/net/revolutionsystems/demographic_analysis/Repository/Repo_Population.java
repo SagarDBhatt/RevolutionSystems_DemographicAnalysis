6a19725a8b36799c8099ae3a813e06a74d50b3ff
Sagar Bhatt <49220046+SagarDBhatt@users.noreply.github.com>
2021-09-18 15:14:10 -0600

    API to fetch the cities within the state is built. Converted List<MAP> into JSON Array to fetch the data. Calculated functions are implemented.

--- src/main/java/net/revolutionsystems/demographic_analysis/Repository/Repo_Population.java
+++ src/main/java/net/revolutionsystems/demographic_analysis/Repository/Repo_Population.java
@@ -2,9 +2,31 @@ package net.revolutionsystems.demographic_analysis.Repository;
 
 import net.revolutionsystems.demographic_analysis.Entity.Population;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-@Repository
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
+
 public interface Repo_Population extends JpaRepository<Population,Long> {
+
+    public String qry_findByCityAndState = "  SELECT * \n" +
+            "  FROM [USPopulation].[dbo].[Population]\n" +
+            "  WHERE City_Name = :city AND State_Name = :state";
+
+    public String qry_findByState = "  SELECT * \n" +
+            "  FROM [USPopulation].[dbo].[Population]\n" +
+            "  WHERE State_Name = :state";
+
+    @Transactional
+    @Modifying
+    @Query(value = qry_findByCityAndState, nativeQuery = true)
+    public List<Map<String,Object>> findByCity_NameAndState_Name(String city, String state);
+
+    @Modifying
+    @Transactional
+    @Query(value = qry_findByState, nativeQuery = true)
+    public List<Map<String,Object>> findByState(String state);
 
 }
