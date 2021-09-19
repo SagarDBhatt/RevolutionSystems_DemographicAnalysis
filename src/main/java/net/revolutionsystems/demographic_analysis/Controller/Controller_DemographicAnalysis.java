dd8cc9bd80c53938629a38073aebf48140686af5
Sagar Bhatt <49220046+SagarDBhatt@users.noreply.github.com>
2021-09-19 02:30:22 -0600

    Functional APIs to generate the opportunity report based on city name and state name.

--- src/main/java/net/revolutionsystems/demographic_analysis/Controller/Controller_DemographicAnalysis.java
+++ src/main/java/net/revolutionsystems/demographic_analysis/Controller/Controller_DemographicAnalysis.java
@@ -1,7 +1,9 @@
 package net.revolutionsystems.demographic_analysis.Controller;
 
+import com.sun.corba.se.impl.orbutil.RepIdDelegator;
 import net.revolutionsystems.demographic_analysis.Entity.Population;
 import net.revolutionsystems.demographic_analysis.Repository.Repo_Population;
+import net.revolutionsystems.demographic_analysis.Utility.ReportCalc;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.management.ObjectName;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -42,20 +45,64 @@ public class Controller_DemographicAnalysis {
     }
 
     @GetMapping(value = "/getCitiesWithinState")
-    public ResponseEntity<List<Map<String, Object>>> get_All_Cities_Within_State(@RequestParam("state") String state){
+    public String get_All_Cities_Within_State(@RequestParam("city") String city, @RequestParam("state") String state) throws IOException {
+        List<Map<String,Object>> list_origin_city = repo_population.findByCity_NameAndState_Name(city,state);
+        JSONArray jsonArray_origin_city = JSONArray.fromObject(list_origin_city);
+        JSONObject jsonObject_origin_city = jsonArray_origin_city.getJSONObject(0);
+
+        ReportCalc.wasteGeneration(jsonObject_origin_city.getDouble("Population"));
+        ReportCalc.processingPricePerTon();
+        ReportCalc.materialRevenue();
+        ReportCalc.pickupCharge(jsonObject_origin_city.getDouble("Population"));
+        ReportCalc.truckingExpense(0);
+        ReportCalc.householdCost(jsonObject_origin_city.getDouble("Population"));
+        ReportCalc.procCost();
+        ReportCalc.netRecycling();
+
+        ReportCalc.writeToFile(jsonObject_origin_city.getDouble("Population"),
+                jsonObject_origin_city.getString("City_Name"),
+                jsonObject_origin_city.getString("State_Name"),
+                true,
+                jsonObject_origin_city.getString("County_Name"),
+                jsonObject_origin_city.getDouble("Latitude"),
+                jsonObject_origin_city.getDouble("Longitude"),
+                0D
+                );
+
         List<Map<String,Object>> cities_within_state = repo_population.findByState(state);
         ResponseEntity<List<Map<String,Object>>> responseEntity_cities_within_state = new ResponseEntity<>(cities_within_state, HttpStatus.OK);
-
         JSONArray jsonArray  = JSONArray.fromObject(cities_within_state);
 
         for(int i=0;i<jsonArray.size();i++) {
             JSONObject jsonObject = jsonArray.getJSONObject(i);
 
-            System.out.println("JSON Array State = " + jsonObject.getString("State_Name"));
-            System.out.println("JSON Array State = " + jsonObject.getString("City_Name"));
+            ReportCalc.wasteGeneration(jsonObject.getDouble("Population"));
+            ReportCalc.processingPricePerTon();
+            ReportCalc.materialRevenue();
+            ReportCalc.pickupCharge(jsonObject.getDouble("Population"));
+
+            Double distance_bet_cities = ReportCalc.distance(jsonObject_origin_city.getDouble("Latitude"), jsonObject_origin_city.getDouble("Longitude"),
+                    jsonObject.getDouble("Latitude"), jsonObject.getDouble("Longitude"));
+
+            ReportCalc.truckingExpense(distance_bet_cities);
+            ReportCalc.householdCost(jsonObject_origin_city.getDouble("Population"));
+            ReportCalc.procCost();
+            ReportCalc.netRecycling();
+
+            ReportCalc.writeToFile(jsonObject.getDouble("Population"),
+                    jsonObject.getString("City_Name"),
+                    jsonObject.getString("State_Name"),
+                    false,
+                    jsonObject.getString("County_Name"),
+                    jsonObject.getDouble("Latitude"),
+                    jsonObject.getDouble("Longitude"),
+                    distance_bet_cities
+            );
+
         }
 
-        return responseEntity_cities_within_state;
-    }
 
-}
+        return "Report generated successfully!!! ";
+    }//End of get_All_Cities_Within_State()
+
+}//End of class Controller_DemographicAnalysis
--- src/main/java/net/revolutionsystems/demographic_analysis/Utility/ReportCalc.java
+++ src/main/java/net/revolutionsystems/demographic_analysis/Utility/ReportCalc.java
@@ -8,70 +8,76 @@ import java.text.DecimalFormat;
 public class ReportCalc {
 
     String oppLocation, oppCounty;
-    double oppPopulation,wstGenerationTPD, wstTPD, processingRevenue, materialRevenue, pickupCharge,
-            truckingExpenseUnbaled, householdCost, processingCost, netRecycling, annualRecycling;
-    final double wstGenerationRate = 3.6;
-    final double wstDiversionRate = 0.3;
-    final double prcPricePerTon = 30;
-    final double targetYield = 0.85;
-    final double mtrlPricePerTon = 157.03;
-    final double peoplePerHouse = 2.4;
-    final double pickupChargePerHH = 1.50;
-    final double processingCostPerTon = 53.58;
+    double oppPopulation;
+    static double wstGenerationTPD;
+    static double wstTPD;
+    static double processingRevenue;
+    static double materialRevenue;
+    static double pickupCharge;
+    static double truckingExpenseUnbaled;
+    static double householdCost;
+    static double processingCost;
+    static double netRecycling;
+    static double annualRecycling;
+    static final double wstGenerationRate = 3.6;
+    static final double wstDiversionRate = 0.3;
+    static final double prcPricePerTon = 30;
+    static final double targetYield = 0.85;
+    static final double mtrlPricePerTon = 157.03;
+    static final double peoplePerHouse = 2.4;
+    static final double pickupChargePerHH = 1.50;
+    static final double processingCostPerTon = 53.58;
 
-    final double truckOperatingCost = 5.32;
-    final double truckingCostUnbaled = 15;
+    static final double truckOperatingCost = 5.32;
+    static final double truckingCostUnbaled = 15;
     final double truckingCostBaled = 21;
 
-    public void wasteGeneration(double cityPopulation, String cityName)
+    public static void wasteGeneration(double cityPopulation)
     {
         wstGenerationTPD = cityPopulation * (wstGenerationRate/2000) * wstDiversionRate;
-
         DecimalFormat df = new DecimalFormat("#.##");
         df.setRoundingMode(RoundingMode.DOWN);
         wstTPD = Double.parseDouble(df.format(wstGenerationTPD));
-
-        System.out.println("Waste Generated = " + wstTPD + " for city = " + cityName);
+        //System.out.println("Waste Generated = " + wstTPD);
     }
 
-    public void processingPricePerTon(String CityName)
+    public static void processingPricePerTon()
     {
         processingRevenue = Double.parseDouble(new DecimalFormat("#.##").format(wstGenerationTPD * prcPricePerTon));
-
-        //JOptionPane.showMessageDialog(null, "Processing Revenue of " + CityName + " is $" + processingRevenue);
+        //System.out.println("Processing Revenue of " + CityName + " is $" + processingRevenue);
     }
 
-    public void materialRevenue(String CityName)
+    public static void materialRevenue()
     {
         materialRevenue = Double.parseDouble(new DecimalFormat("#.##").format(wstGenerationTPD * targetYield * mtrlPricePerTon));
-        //JOptionPane.showMessageDialog(null, "Material Revenue generated in" + CityName + " is $" + materialRevenue);
+        //System.out.println("Material Revenue generated in" + CityName + " is $" + materialRevenue);
     }
 
-    public void pickupCharge(double cityPopulation, String cityName)
+    public static void pickupCharge(double cityPopulation)
     {
         pickupCharge = Double.parseDouble(new DecimalFormat("#.##").format((cityPopulation/peoplePerHouse) * (pickupChargePerHH)/30));
-        //JOptionPane.showMessageDialog(null, "Pickup charge per House hold in " + cityName + " $" + pickupCharge + " daily");
+        //System.out.println("Pickup charge per House hold in " + cityName + " $" + pickupCharge + " daily");
     }
 
-    public void truckingExpense(double distanceBetCities)
+    public static void truckingExpense(double distanceBetCities)
     {
         truckingExpenseUnbaled = Double.parseDouble(new DecimalFormat("#.##").format((2*distanceBetCities*truckOperatingCost)/truckingCostUnbaled*wstGenerationTPD));
         //JOptionPane.showMessageDialog(null, "Trucking expense to transport unbaled material is $" + truckingExpenseUnbaled);
     }
 
-    public void householdCost(double cityPopulation)
+    public static void householdCost(double cityPopulation)
     {
         householdCost = Double.parseDouble(new DecimalFormat("#.##").format((processingRevenue + truckingExpenseUnbaled)/((cityPopulation/peoplePerHouse))*30));
         //JOptionPane.showMessageDialog(null, "House hold cost $" + householdCost);
     }
 
-    public void procCost()
+    public static void procCost()
     {
         processingCost = Double.parseDouble(new DecimalFormat("#.##").format((wstGenerationTPD * processingCostPerTon)));
         //JOptionPane.showMessageDialog(null, "Processing Cost $" + processingCost);
     }
 
-    public void netRecycling()
+    public static void netRecycling()
     {
         netRecycling = Double.parseDouble(new DecimalFormat("#.##").format(processingRevenue + materialRevenue + pickupCharge - processingCost));
         //JOptionPane.showMessageDialog(null, "Net value of Recycling $" + netRecycling);
@@ -80,26 +86,23 @@ public class ReportCalc {
         //JOptionPane.showMessageDialog(null, "Annualized Recycling revenue $" + annualRecycling);
     }
 
-//    public void writeToFile(double cityPopulation, String cityName, String State, boolean FC,String Counties, double lati, double longi) throws IOException, IOException {
-//        String fileContent = "";
-//        String fileCont = "";
-//
-//        if(FC == true)
-//        {
-//            //fileContent = "Opportunity Survey of ::::  " + cityName + "\n\n";
-//            fileContent += 	"City\t County\t State\t Population\t Recyclable Material (TPD)\t Processing Revenue\t Material Revenue\t Pickup Revenue\t Distance\t Truck expense\t House hold cost\t Processing Cost\t Net Recycling\t Annualized\t Latitude\t Longitude " + "\n\n";
-//            //filename = "OppSurveyfor__"+ cityName + ".txt";
-//        }
-//
-//        fileContent += cityName + "\t"+ Counties +  "\t" + State + "\t" + cityPopulation + "\t" + wstTPD +"\t" + processingRevenue +"\t" + materialRevenue +"\t" + pickupCharge +"\t" + oppCityNameObject.distanceBetCities + "\t"+ truckingExpenseUnbaled +"\t"+ householdCost +"\t" + processingCost +"\t" + netRecycling +"\t" + annualRecycling + "\t" + lati + "\t" + longi + "\n";
-//
-//        FileWriter fw = new FileWriter("OpportunityReport.txt",true);
-//        fw.write(fileContent);
-//        fw.close();
-//    }
+    public static void writeToFile(double cityPopulation, String cityName, String State, boolean column_Headers,String Counties, double lati, double longi, Double distBetCities) throws IOException, IOException {
+        String fileContent = "";
+        String fileCont = "";
 
-    public double distance(double lat1, double lon1, double lat2, double lon2) {
-        /* System.out.println("xxxx"); */
+        if(column_Headers == true)
+        {
+            fileContent += 	"City\t County\t State\t Population\t Recyclable Material (TPD)\t Processing Revenue\t Material Revenue\t Pickup Revenue\t Distance\t Truck expense\t House hold cost\t Processing Cost\t Net Recycling\t Annualized\t Latitude\t Longitude " + "\n\n";
+        }
+
+        fileContent += cityName + "\t"+ Counties +  "\t" + State + "\t" + cityPopulation + "\t" + wstTPD +"\t" + processingRevenue +"\t" + materialRevenue +"\t" + pickupCharge +"\t" + distBetCities + "\t"+ truckingExpenseUnbaled +"\t"+ householdCost +"\t" + processingCost +"\t" + netRecycling +"\t" + annualRecycling + "\t" + lati + "\t" + longi + "\n";
+
+        FileWriter fw = new FileWriter("OpportunityReport.txt",true);
+        fw.write(fileContent);
+        fw.close();
+    }
+
+    public static double distance(double lat1, double lon1, double lat2, double lon2) {
 
         if ((lat1 == lat2) && (lon1 == lon2)) {
             return 0;
