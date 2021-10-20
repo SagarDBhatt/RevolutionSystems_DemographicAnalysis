4444b7001963c675314e094a5a6f0b55d13826a3
Sagar Bhatt <49220046+SagarDBhatt@users.noreply.github.com>
2021-10-19 22:50:01 -0600

    Create an API endpoint to fetch all the cities. Added native query in the JPA repository

--- src/main/java/net/revolutionsystems/demographic_analysis/Repository/Repo_Population.java
+++ src/main/java/net/revolutionsystems/demographic_analysis/Repository/Repo_Population.java
@@ -22,7 +22,11 @@ public interface Repo_Population extends JpaRepository<Population,Long> {
     public String qry_findAllCities = "  SELECT * \n" +
             "  FROM [USPopulation].[dbo].[Population]";
 
-    public String qry_getAllStates = "SELECT DISTINCT State_Name FROM [USPopulation].[dbo].[Population]";
+    public String qry_getAllStates = "SELECT DISTINCT State_Name FROM [USPopulation].[dbo].[Population] ORDER BY State_Name";
+
+    public String qry_getAllCitiesWithinState = "SELECT DISTINCT City_Name FROM [USPopulation].[dbo].[Population] WHERE State_Name = :state";
+
+    public String qry_getAllcities = "SELECT DISTINCT City_Name FROM [USPopulation].[dbo].[Population] ORDER BY City_Name";
 
     @Transactional
     @Modifying
@@ -43,4 +47,14 @@ public interface Repo_Population extends JpaRepository<Population,Long> {
     @Modifying
     @Query(value = qry_getAllStates, nativeQuery = true)
     public List<Map<String,Object>> findAllStates();
+
+    @Transactional
+    @Modifying
+    @Query(value = qry_getAllCitiesWithinState,nativeQuery = true)
+    public List<Map<String,Object>> findAllCitiesWithinState(String state);
+
+    @Modifying
+    @Transactional
+    @Query(value = qry_getAllcities, nativeQuery = true)
+    public List<Map<String,Object>> getAllCities();
 }
